ff1ebd4d32934612be20b2c2d5d1ab5cdb92c13b
Vinicius Oliveira <viniciusoa@outlook.com>
2024-01-21 14:43:37 -0300

    feat: create controller

---
+++ controllers/controller.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	db "github.com/vinolivae/personalitiesAPI/database"
+	m "github.com/vinolivae/personalitiesAPI/models"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
+}
+
+func ListPersonalities(w http.ResponseWriter, r *http.Request) {
+	var personalities []m.Personality
+	db.DB.Find(&personalities)
+
+	// Encoda uma resposta em json, e a resposta a ser encodada é "personalities".
+	json.NewEncoder(w).Encode(personalities)
+}
+
+func DetailPersonality(w http.ResponseWriter, r *http.Request) {
+	var personality []m.Personality
+
+	//Retorna as variáveis da rota, quero o id.
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	db.DB.First(&personality, id)
+
+	json.NewEncoder(w).Encode(personality)
+}
