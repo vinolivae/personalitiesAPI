9b808d3286763db34d5a16626261ab953903d9ea
Vinicius Oliveira <viniciusoa@outlook.com>
2024-01-23 21:48:17 -0300

    chore: use middleware

--- router/router.go
+++ router/router.go
@@ -6,14 +6,19 @@ import (
 
 	"github.com/gorilla/mux"
 	c "github.com/vinolivae/personalitiesAPI/controllers"
+	m "github.com/vinolivae/personalitiesAPI/middleware"
 )
 
 func LoadRoutes() {
 	r := mux.NewRouter()
+	r.Use(m.ContentTypeMiddleware)
 
 	r.HandleFunc("/", c.Home)
 	r.HandleFunc("/api/personalities", c.ListPersonalities).Methods("GET")
 	r.HandleFunc("/api/personalities/{id}", c.DetailPersonality).Methods("GET")
+	r.HandleFunc("/api/personalities", c.CreatePersonality).Methods("POST")
+	r.HandleFunc("/api/personalities/{id}", c.DeletePersonality).Methods("DELETE")
+	r.HandleFunc("/api/personalities/{id}", c.UpdatePersonality).Methods("PUT")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
